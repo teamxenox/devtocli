548f6b7c29f1a2959bf63d96dd778a6537efe987
Sarthak Sharma <7lovesharma7@gmail.com>
2019-01-25 19:03:07 +0530

    Search by tag

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 const crawler = require('./util/crawler');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
-const opn = require('opn')
+const opn = require('opn');
+const program = require('commander');
 
 let articles;
 
-crawler.fetchHome().then(data => {
-    articles = data.filter(data => data.title != undefined);
-    askQuestion(articles.map(data => data.title));
-})
+program
+    .version('0.0.0')
 
+
+program
+    .command("tag <tag>")
+    .alias("l")
+    .action((tag) => {
+        crawler.fetchByTags(tag).then(data => {
+            articles = data.filter(data => data.title != undefined);
+            askQuestion(articles.map(data => data.title));
+        });
+    })
+
+program.parse(process.argv);
+
+if(program.args.length === 0){
+    crawler.fetchHome().then(data => {
+        articles = data.filter(data => data.title != undefined);
+        askQuestion(articles.map(data => data.title));
+    })
+} 
 
 const askQuestion = (titles) => {
-    inquirer.prompt({
+    inquirer.prompt([{
             type: 'list',
             name: 'title',
-            message: 'select the story 🤓',
+            message: 'Select the story 🤓',
             choices: titles,
             paginated: true
-        })
+        }])
         .then(answers => {
             openLink(answers);
         });
 }
 
 const openLink = (answers) => {
-      opn(articles.find(data=>data.title === answers.title).link);
+    opn(articles.find(data => data.title === answers.title).link);
+}
+
+const showArticle = (answers) => {
+    crawler.fetchArticle(articles.find(data => data.title === answers.title).link);
 }
--- util/crawler.js
+++ util/crawler.js
@@ -26,4 +26,30 @@ const fetchHome =()=>{
         tag:['.tags .tag | trim']
       }]);
 } 
-module.exports = { fetchHome };
+
+/**
+ * This is a function to fetch feed by tags of `dev.to` using the `x-ray` module.
+ * @param {string} tag - Tag by which articles will be fetched
+ * @returns {Promise<Array>} The promise with data scrapped from webpage.
+ */
+
+const fetchByTags =(tag)=>{
+    return xray('https://dev.to/t/'+tag, '#substories .single-article', [{
+        title: '.index-article-link .content h3 | trim',
+        author: 'h4 a | trim',
+        link:'.index-article-link@href',
+        tag:['.tags .tag | trim']
+      }]);
+} 
+
+/**
+ * This is a function to fetch Article from link.
+ * @param {string} url - url of article to fetched
+ * @returns {Promise<Array>} The promise with data scrapped from webpage.
+ */
+
+const fetchArticle =(url)=>{
+    return xray(url, '#article-body | trim').then(data=>console.log(data));
+} 
+
+module.exports = { fetchHome , fetchArticle , fetchByTags};
