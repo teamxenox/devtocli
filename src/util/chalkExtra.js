c00ca248c09d3f2c6ce09169a451ba43cf74bece
Riguidix Rodz <riguidixrodriguez@gmail.com>
2019-07-27 19:41:33 -0600

    Setting the Colors of Bootstrap as Template

---
+++ src/util/chalkExtra.js
@@ -0,0 +1,11 @@
+/*
+ * This File will Handle a set of functions for remove the complexity or verbosity of
+ ** Console.log( chalk.background.foreground( "Long String Text" ) );
+ */
+
+'use strict';
+
+const chalk = require('chalk');
+
+// Bootstrap Colors Template
+const primary = "#007bff", secondary = '#6c757d', success = '#28a745', info = '#17a2b8', warning = '#ffc107', danger = '#dc3545', light = '#f8f9fa', dark = '#343a40';
