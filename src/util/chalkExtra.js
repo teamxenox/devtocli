a2f4a6eb50a0618668cf2c99c09248a6c5ef30ca
Riguidix Rodz <riguidixrodriguez@gmail.com>
2019-07-27 19:56:50 -0600

    Implementing for output the correctly Type

--- src/util/chalkExtra.js
+++ src/util/chalkExtra.js
@@ -16,8 +16,46 @@ const warning = console.warn;
 // Bootstrap Colors Template
 const primary = "#007bff", secondary = '#6c757d', success = '#28a745', info = '#17a2b8', warning = '#ffc107', danger = '#dc3545', light = '#f8f9fa', dark = '#343a40';
 
-function Log(text, type) {
+// Types of Messages
+const primaryMsg     = chalk.bold.bgHex(primary).hex(light);
+const secondaryMsg   = chalk.bold.bgHex(secondary).hex(light);
+const successMsg     = chalk.bold.bgHex(success).hex(light);
+const infoMsg        = chalk.bold.bgHex(info).hex(light);
+const errorMsg       = chalk.bold.bgHex(danger).hex(light);
+const warningMsg     = chalk.bgHex(warning).hex(dark);
+const darkMsg        = chalk.bold.bgHex(dark).hex(light);
 
+/**
+ * This is a function to display logs in terminal.
+ * @param {string} text - Text to display in the terminal
+ * @param {string} type - Define the type of log
+ * @returns {null} null
+ */
+
+function Log(text, type) {
+    switch(type.toLowerCase()) {
+        case "primary":
+            console.log(primaryMsg(text));
+            break;
+        case "secondary":
+            console.log(secondaryMsg(text));
+            break;
+        case "success":
+            console.log(successMsg("Success:", text));
+            break;
+        case "info":
+            console.info(infoMsg("Info:", text));
+            break;
+        case "error":
+            console.error(errorMsg("Error:", text));
+            break;
+        case "warning":
+            console.log(warningMsg("Warning:", text));
+            break;
+        case "dark":
+            console.log(darkMsg(text));
+            break;
+    }
 }
 
 exports.Log = Log;
